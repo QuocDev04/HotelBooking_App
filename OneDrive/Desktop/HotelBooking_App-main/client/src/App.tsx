ffbe5d3b8891236d6a659e7f2e5f3512d54a1044
toanltph49436 <toanltph49436@gmail.com>
2025-06-09 13:04:23 +0700

    Sua

--- OneDrive/Desktop/HotelBooking_App-main/client/src/App.tsx
+++ OneDrive/Desktop/HotelBooking_App-main/client/src/App.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import FlightBooking from './pages/fight-booking'
+import Layout from './compoments/Layout'
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path="/flight-booking" element={<FlightBooking />} />
-
-        
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path="/flight-booking" element={<FlightBooking />} />
+        </Routes>
+      </Layout>
     </Router>
   )
 }
