19cab4b3930328a4ff7b427a9c3bbaad618d5d1b
toanltph49436 <toanltph49436@gmail.com>
2025-06-09 17:06:38 +0700

    may bay

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Layout from './compoments/layout'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import  CruisePage from './pages/cruise.tsx'
+import FlightBooking from './pages/FlightBooking.tsx'
 const App = () => {
   return (
     <Router>
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cruise" element={<CruisePage />} />
+          <Route path="/flight-booking" element={<FlightBooking />} />
         </Routes>
       </Layout>
     </Router>
