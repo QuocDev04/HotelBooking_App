c744b0c1857b792be118c99800bab44d18be4dc0
toanltph49436 <toanltph49436@gmail.com>
2025-06-09 12:37:02 +0700

    app.tsx

--- OneDrive/Desktop/HotelBooking_App-main/client/src/App.tsx
+++ OneDrive/Desktop/HotelBooking_App-main/client/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
-    <div className='bg-red-500'>
-      app
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path="/flight-booking" element={<FlightBooking />} />
+
+        
+      </Routes>
+    </Router>
   )
 }
 
