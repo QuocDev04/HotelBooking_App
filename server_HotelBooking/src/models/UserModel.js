c75216e66247e927ee841c4e94d8dbf3596a1f90
Nguyễn Quốc <huyquoc2xx4@gmail.com>
2025-06-10 08:06:46 +0700

    UserSchema

---
+++ server_HotelBooking/src/models/UserModel.js
@@ -0,0 +1,13 @@
+
+import mongoose from 'mongoose';
+
+const UserModel = new mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone_number: { type: String, required: true },
+    address: { type: String },
+    profile_picture: { type: String }
+}, { timestamps: true })
+
+export default mongoose.model("User", UserModel)
