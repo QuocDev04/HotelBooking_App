ac725bb8633dd271c7b6fb82672acf5ca2bca71a
Nguyễn Quốc <huyquoc2xx4@gmail.com>
2025-06-13 15:21:01 +0700

    done

--- HotelBooking_App/client/src/router/index.tsx
+++ HotelBooking_App/client/src/router/index.tsx
@@ -2,11 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import LayoutPages from "../pages/LayoutPages"
 import Home from "../pages/Home/LayoutHome"
 import InfoAll from "../pages/Introduce/InfoAll"
-<<<<<<< HEAD
 import YachtDetailPage from "../pages/Book/yacht"
-
-
-=======
 import RoomList from "../pages/Tours/Rooms"
 import Transport from "../pages/Tours/Transport"
 import DestinationList from "../pages/Tours/DestinationList"
@@ -18,16 +14,13 @@ import { HotelPolicy } from "../pages/Blog/hotelPolicy"
 import JapanTourPage from "../pages/Tour/detailTour"
 import BookingRoom from "../pages/Booking/BookingRoom"
 import BookingAll from "../pages/Booking/BookingAll"
->>>>>>> origin/main
 const Router = () => {
     return (
          <Routes>
             <Route path="/" element={<LayoutPages/>}>
                 <Route path="/" element={<Home />} />
                 <Route path="/introduce" element={<InfoAll />} />
-<<<<<<< HEAD
                 <Route path="/d" element={<YachtDetailPage />} />
-=======
                <Route path="/rooms" element={<RoomList />} />
                 <Route path="/transport" element={<Transport />} />
                 <Route path="/destinations" element={<DestinationList />} />
@@ -39,7 +32,6 @@ const Router = () => {
                 <Route path="/detailtour/:id" element={<JapanTourPage />} />
                 <Route path="/bookingroom" element={<BookingRoom />} />
                 <Route path="/bookingall" element={<BookingAll />} />
->>>>>>> origin/main
             </Route>
         </Routes>
     )
