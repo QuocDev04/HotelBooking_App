88ec4cbf93be3bb76b28873db2c129eaf594e0b6
Nguyễn Quốc <huyquoc2xx4@gmail.com>
2025-07-07 20:43:39 +0700

    updsate

--- HotelBooking_App/server_HotelBooking/src/app.js
+++ HotelBooking_App/server_HotelBooking/src/app.js
@@ -1,22 +1,20 @@
-
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import mongoose from "mongoose";
 import connectDB from "./config/mongodb";
 import Vnpay from "./router/vnpay/vnpayRouter.js";
 import RouterBookingTour from './router/TourRouter/BookingTour';
 import RouterBookingOnly from './router/RoomRouer/BookingOnLyRouter';
 import AdminRouter from './router/PeopleRouter/AdminRouter';
 import TourSchedule from './router/TourRouter/TourScheduleRouter';
-import TransportSchedulemodel from './router/TransportRouter/TransportScheduleModel';
+import TransportScheduleRouter from './router/TransportRouter/TransportScheduleModel';
 import TransportRouter from './router/TransportRouter/TransportRouter';
 import TourRouter from './router/TourRouter/TourRouter';
 import RouterRoom from './router/RoomRouer/RoomRouter';
 import UserRouter from './router/PeopleRouter/UserRouter';
 import RouterChecOutBookingTour from "./router/TourRouter/checkOutTour";
 import routerLocation from "./router/Location/locationRouter";
-import CmtRouter from "./router/Cmt/CmtRouter.js";
+import CmtRouter from "./router/Cmt/CmtRouter";
 
 const app = express();
 app.use(express.json());
@@ -30,7 +28,7 @@ app.use('/api', UserRouter)
 app.use('/api/', RouterRoom)
 app.use('/api/', TourRouter)
 app.use('/api/', TransportRouter)
-app.use('/api/', TransportSchedulemodel)
+app.use('/api/', TransportScheduleRouter)
 app.use('/api/', TourSchedule)
 app.use('/api/', AdminRouter)
 app.use('/api/', RouterBookingOnly)
@@ -39,5 +37,10 @@ app.use('/api/', RouterBookingTour)
 app.use('/api/', RouterChecOutBookingTour)
 app.use('/api/', routerLocation)
 app.use('/api/', CmtRouter)
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error', message: err.message });
+});
 
 export const viteNodeApp = app;
