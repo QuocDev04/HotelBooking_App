c3b5533185e8aba3c23aa572e860d0b2fd14b6cf
Nguyễn Quốc <huyquoc2xx4@gmail.com>
2025-07-08 08:04:08 +0700

    update

--- HotelBooking_App/client/configs/instance.tsx
+++ HotelBooking_App/client/configs/instance.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const base = import.meta.env.DEV
     ? 'http://localhost:8080/api/'
-    : 'https://hotel-booking-app-server-eight.vercel.app/';
+    : 'https://hotel-booking-app-server-eight.vercel.app/api/';
 
 const instanceClient = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL || base
