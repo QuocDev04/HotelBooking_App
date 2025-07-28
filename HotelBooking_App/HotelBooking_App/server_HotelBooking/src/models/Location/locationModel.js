751c34497c6d19e05013ccac989e55f6109ba36b
ntminh <minhntph34147@fpt.edu.vn>
2025-07-28 07:23:40 +0700

    "80"

--- HotelBooking_App/HotelBooking_App/server_HotelBooking/src/models/Location/locationModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const LocationSchema = new mongoose.Schema({
-    locationName: { type: String, required: true },
-    country: { type: String, required: true }
-}, { timestamps: true })
-
-module.exports = mongoose.model("Location", LocationSchema)
