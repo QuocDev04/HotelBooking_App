6670fdc1fc6fd310592a89a2c57a63a8cd294a41
toanltph49436 <toanltph49436@gmail.com>
2025-08-02 06:16:50 +0700

    cap nhat

--- HotelBooking_App/server_HotelBooking/src/router/TourRouter/checkOutTour.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { checkOutBookingTour } from '../../controller/TourController/checkOutTour';
-const RouterChecOutBookingTour = express.Router();
-RouterChecOutBookingTour.post('/checkOutBookingTour/:id', checkOutBookingTour)
-
-export default RouterChecOutBookingTour
