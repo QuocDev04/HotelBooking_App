ae566ced50ec74a049c036174220c5e37e7e100f
Nguyễn Quốc <huyquoc2xx4@gmail.com>
2025-09-01 08:35:52 +0700

    vercel

--- HotelBooking_App/server_HotelBooking/src/models/People/EmployeeModel.js
+++ HotelBooking_App/server_HotelBooking/src/models/People/EmployeeModel.js
@@ -94,7 +94,6 @@ EmployeeSchema.pre('save', function (next) {
 });
 
 // Index để tìm kiếm nhanh
-EmployeeSchema.index({ email: 1 });
 EmployeeSchema.index({ status: 1 });
 EmployeeSchema.index({ position: 1 });
 EmployeeSchema.index({ department: 1 });
